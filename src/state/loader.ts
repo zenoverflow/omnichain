3b985ecc071edeb066cd4ba2d9e90b653b7605c8
Zen <zenoverflow@pm.me>
2024-04-30 14:43:37 +0300

    Deferred global loader start & stop

--- src/state/loader.ts
+++ src/state/loader.ts
@@ -2,10 +2,29 @@ import { StatefulObservable } from "../util/ObservableUtils";
 
 export const loaderStorage = new StatefulObservable<boolean>(false);
 
+let startTimeout: NodeJS.Timeout | null = null;
+let finishTimeout: NodeJS.Timeout | null = null;
+
 export const startGlobalLoading = () => {
-    loaderStorage.set(true);
+    if (startTimeout) {
+        clearTimeout(startTimeout);
+    }
+    if (finishTimeout) {
+        clearTimeout(finishTimeout);
+    }
+    startTimeout = setTimeout(() => {
+        loaderStorage.set(true);
+    }, 120);
 };
 
 export const finishGlobalLoading = () => {
-    loaderStorage.set(false);
+    if (startTimeout) {
+        clearTimeout(startTimeout);
+    }
+    if (finishTimeout) {
+        clearTimeout(finishTimeout);
+    }
+    finishTimeout = setTimeout(() => {
+        loaderStorage.set(false);
+    }, 1000);
 };
